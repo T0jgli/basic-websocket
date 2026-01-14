73b2b6ef1a68053126aca5ecd13911277fc46deb
T0jgli <kovalikadam00@gmail.com>
2026-01-14 23:53:58 +0100

    upgrade

--- src/index.ts
+++ src/index.ts
@@ -7,38 +7,42 @@ import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
+
 let onlineUsers = new UsersRepository();
 
 const server = http.createServer(app);
 const io = new socketIO.Server(server, {
-    cors: {
-        origin: process.env.ORIGINS?.split(" "),
-        methods: ["GET", "POST"],
-    },
+  cors: {
+    origin: process.env.ORIGINS?.split(" "),
+    methods: ["GET", "POST"],
+  },
 });
 
 io.on("connection", (socket: socketIO.Socket) => {
-    socket.on("onlineUser", ({ details, isMobile }) => {
-        onlineUsers.pushUser(new User(details, socket.id, isMobile));
+  socket.on("onlineUser", ({ details, isMobile }) => {
+    onlineUsers.pushUser(new User(details, socket.id, isMobile));
 
-        io.emit("usersChanged", {
-            onlineUsers: onlineUsers.getUsers,
-        });
+    io.emit("usersChanged", {
+      onlineUsers: onlineUsers.getUsers,
     });
+  });
 
-    socket.on("disconnect", () => {
-        try {
-            onlineUsers.removeUser(socket.id);
+  socket.on("disconnect", () => {
+    try {
+      onlineUsers.removeUser(socket.id);
 
-            io.emit("usersChanged", {
-                onlineUsers: onlineUsers.getUsers,
-            });
-        } catch (e) {
-            return new Error(e as any);
-        }
-    });
+      io.emit("usersChanged", {
+        onlineUsers: onlineUsers.getUsers,
+      });
+    } catch (e) {
+      return new Error(e as any);
+    }
+  });
 });
 
 server.listen(process.env.port || 3000, () => {
-    console.log(`App running on port ${process.env.port || 3000}`);
+  console.log(`App running on port ${process.env.port || 3000}`);
 });
